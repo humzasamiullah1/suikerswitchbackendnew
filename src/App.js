bba8f85c42da062bf849197c8ab22a0e26cc3dfd
Hassan <hassanot234@gmail.com>
2025-02-18 18:58:08 +0500

    forget password screen done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Verification from "./pages/Verification"
 import Dashboard from "./pages/Dashboard";
+import ForgetPassword from "./pages/ForgetPass"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/verification" element={<Verification />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/dashboard/*" element={<Dashboard />} />
       </Routes>
     </Router>
---
+++ src/pages/ForgetPass.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import ImageTag from "../components/reuseable/imageTag"
+import FormSection from "../components/forget-password/loginForm"
+
+function ForgetPassword() {
+
+  return (
+    <div className="lg:h-screen flex items-center">
+      <div className="flex w-full lg:w-[90%] mx-auto lg:h-[90vh] rounded-xl">
+        <div className="w-[50%] h-full hidden lg:block">
+          <ImageTag path='/assets/images/auth/auth-image.png' classes='w-full h-full' altText='login' />
+        </div>
+        <div className="lg:w-[50%] w-full h-screen lg:h-full my-auto flex flex-col items-center justify-center bg-white">
+          <div className="w-[80%] mx-auto flex flex-col items-center justify-center">
+            <ImageTag path='/assets/images/logo.png' classes='w-full lg:w-[80%] h-20' altText='logo' />
+            <div className="w-full pt-16 h-full">
+              <h1 className="text-fontColor font-HelveticaNeueMedium font-bold xl:text-2xl 2xl:text-3xl">Forgot Password</h1>
+              <p className="text-fontColor text-xs pt-1 font-HelveticaNeueRegular">Welcome back! , Please put your login credentials below to start using the dashboard.</p>
+              <FormSection/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ForgetPassword;
