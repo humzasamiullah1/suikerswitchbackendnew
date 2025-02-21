c5a3aa84eac58c0e76a8cf89385dfee12b252ec0
Hassan <hassanot234@gmail.com>
2025-02-21 19:59:10 +0500

    add product page ui done

--- src/components/add-product/FormSection.jsx
+++ src/components/add-product/FormSection.jsx
@@ -1,27 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import LabelTag from "../reuseable/label";
-import { motion } from "framer-motion";
+import { Plus } from "lucide-react";
 
 const FormSection = () => {
+  const [images, setImages] = useState([]);
+
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    const imageUrls = files.map((file) => URL.createObjectURL(file));
+    setImages((prevImages) => [...prevImages, ...imageUrls]);
+  };
+
   return (
-    <form>
-      <div className="flex justify-between">
-        <div className="w-[49%]">
-          <LabelTag name="Product Name" classes="!text-sm" />
+    <form className="border-2 border-gray-100 rounded-2xl w-full px-3 py-4 mt-3">
+      <div className="p-4">
+        <h2 className="text-lg font-HelveticaNeueMedium mb-2">Images</h2>
+        <div className="flex space-x-2">
+          {images.map((image, index) => (
+            <div key={index} className="w-20 h-20 rounded-lg overflow-hidden">
+              <img
+                src={image}
+                alt={`Selected ${index}`}
+                className="w-full h-full object-cover"
+              />
+            </div>
+          ))}
+          <label className="w-20 h-20 flex items-center justify-center bg-red-100 text-red-500 rounded-lg cursor-pointer">
+            <Plus size={24} />
+            <input
+              type="file"
+              accept="image/*"
+              multiple
+              className="hidden"
+              onChange={handleImageChange}
+            />
+          </label>
+        </div>
+      </div>
+      <div className="flex flex-col md:flex-row justify-between pt-5">
+        <div className="w-full md:w-[49%]">
+          <LabelTag isStaric={false} name="Product Name" classes="!text-sm" />
           <input
             type="text"
             placeholder="Write Product Name"
             className="w-full mt-1 text-sm font-popinsRegular rounded-md bg-bgColor px-3 py-[10px] text-darkColor placeholder:text-zinc-700/50"
           />
         </div>
-        <div className="w-[49%]">
-          <LabelTag name="Product Price" classes="!text-sm" />
+        <div className="w-full md:w-[49%] pt-4 md:pt-0">
+          <LabelTag isStaric={false} name="Product Price" classes="!text-sm" />
           <input
             type="text"
             placeholder="Write Product Name"
             className="w-full mt-1 text-sm font-popinsRegular rounded-md bg-bgColor px-3 py-[10px] text-darkColor placeholder:text-zinc-700/50"
           />
         </div>
+      </div>
+      <div className="flex flex-col md:flex-row justify-between mt-4">
+        <div className="w-full md:w-[49%]">
+          <LabelTag isStaric={false} name="Select Brand" classes="!text-sm" />
+          <select className="w-full mt-1 text-sm bg-bgColor font-popinsRegular px-3 py-2 rounded-lg text-darkColor placeholder:text-zinc-700/50">
+            <option>Select Brand</option>
+          </select>
+        </div>
+        <div className="w-full md:w-[49%] pt-4 md:pt-0">
+          <LabelTag
+            isStaric={false}
+            name="Select Category"
+            classes="!text-sm"
+          />
+          <select className="w-full mt-1 text-sm bg-bgColor font-popinsRegular px-3 py-2 rounded-lg text-darkColor placeholder:text-zinc-700/50">
+            <option>Select Category</option>
+          </select>
+        </div>
+      </div>
+      <div className="pt-2 w-full mt-4">
+        <LabelTag isStaric={false} name="Description" classes="!text-sm" />
+        <textarea
+          type="text"
+          cols={10}
+          placeholder="Write Product Description"
+          className="w-full h-28 mt-1 text-sm bg-bgColor font-popinsRegular px-3 py-2 rounded-lg text-darkColor placeholder:text-zinc-700/50"
+        ></textarea>
       </div>
     </form>
   );
--- src/components/add-product/mainAdd.jsx
+++ src/components/add-product/mainAdd.jsx
@@ -8,20 +8,20 @@ const MainAddProucts = () => {
       initial={{ opacity: 0, y: 20 }} // Pehle invisible aur neeche se aayega
       animate={{ opacity: 1, y: 0 }} // Phir smoothly dikhai dega
       transition={{ duration: 0.6, ease: "easeOut" }} // 0.6s ka smooth transition
-      className="bg-white rounded-[30px] shadow-md px-5 h-full"
+      className="bg-white rounded-[30px] shadow-md px-5 h-full mb-5 lg:mb-0"
     >
       <div className="flex lg:flex-row flex-col border-b-2 border-gray-100 justify-between items-center pt-2 lg:h-[12%]">
-        <div className="flex justify-between w-full items-center lg:w-[30%] xl:w-[50%]">
+        <div className="flex justify-center lg:justify-start w-full items-center lg:w-[30%] xl:w-[50%]">
           <p className="font-HelveticaNeueMedium text-darkColor text-lg">
             Product Information
           </p>
         </div>
-        <div className="flex items-center lg:w-[70%] xl:w-[50%] justify-end">
+        <div className="flex items-center lg:w-[70%] xl:w-[50%] justify-end pb-3 lg:pb-0">
           <div className="flex items-center gap-2 mt-3 md:mt-0">
             <button className="border text-xs rounded-full px-4 py-2 flex items-center font-HelveticaNeueMedium text-darkColor bg-gray-200 hover:bg-gray-200">
               Cancel
             </button>
-            <button className="border text-xs hidden rounded-full px-4 w-full py-2 md:flex items-center font-HelveticaNeueMedium text-white bg-gkRedColor hover:bg-gkRedColor/90">
+            <button className="border text-xs rounded-full px-4 w-full py-2 flex items-center font-HelveticaNeueMedium text-white bg-gkRedColor hover:bg-gkRedColor/90">
               Proceed
             </button>
           </div>
