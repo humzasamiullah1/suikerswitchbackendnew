1e9bf0e7a695db69aa25fb2e6dbfce54a6ee35da
humzasamiullah1 <95347372+humzasamiullah1@users.noreply.github.com>
2025-06-23 22:11:25 +0500

    updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import ImageTag from "../components/reuseable/imageTag"
-import FormSection from "../components/login-form/loginForm"
+import ImageTag from "../components/reuseable/imageTag";
+import FormSection from "../components/login-form/loginForm";
 
 function Login() {
   const navigate = useNavigate();
@@ -10,18 +10,28 @@ function Login() {
   };
 
   return (
-    <div className="lg:h-screen flex items-center">
+    <div className="lg:h-screen flex items-center bg-white">
       <div className="flex w-full lg:w-[90%] mx-auto lg:h-[90vh] rounded-xl">
-        <div className="w-[50%] h-full hidden lg:block">
-          <ImageTag path='/assets/images/auth/auth-image.png' classes='w-full h-full' altText='login' />
-        </div>
-        <div className="lg:w-[50%] w-full h-screen lg:h-full my-auto flex flex-col items-center justify-center bg-white">
+        {/* <div className="w-[50%] h-full hidden lg:block">
+          <ImageTag
+            path="/assets/images/auth/auth-image.png"
+            classes="w-full h-full"
+            altText="login"
+          />
+        </div> */}
+        <div className=" w-full h-screen lg:h-full my-auto flex flex-col items-center justify-center bg-white">
           <div className="w-[80%] mx-auto flex flex-col items-center justify-center">
-            <ImageTag path='/assets/images/logo.png' classes='w-[80%] h-16 md:w-[60%] lg:w-[80%] md:h-20' altText='logo' />
-            <div className="w-full pt-16 h-full">
-              <h1 className="text-fontColor font-HelveticaNeueMedium font-bold xl:text-2xl 2xl:text-3xl">Log in</h1>
-              <p className="text-fontColor text-xs pt-1 font-HelveticaNeueRegular">Welcome back! , Please put your login credentials below to start using the dashboard.</p>
-              <FormSection/>
+            <ImageTag
+              path="/assets/images/paymentlogo.jpeg"
+              classes="w-[80%] h-[140px] object-contain "
+              altText="logo"
+            />
+            <div className="w-full pt-8 h-full flex items-center justify-center">
+              {/* <h1 className="text-fontColor font-HelveticaNeueMedium font-bold xl:text-2xl 2xl:text-3xl">
+                Log in
+              </h1> */}
+              {/* <p className="text-fontColor text-xs pt-1 font-HelveticaNeueRegular">Welcome back! , Please put your login credentials below to start using the dashboard.</p> */}
+              <FormSection />
             </div>
           </div>
         </div>
